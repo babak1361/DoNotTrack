5450f19ee1e5ee384e90e783f2ae0916665a68ae
babak <babak.nikahval.bn@gmail.com>
2022-01-11 15:26:58 -0500

    four commit

--- src/WoodenSpoon.java
+++ src/WoodenSpoon.java
@@ -4,3 +4,16 @@ public class WoodenSpoon {
 
     }
 }
+/*
+## How to Remove version control from The project you did not intented to keep track of.
+
+1. Create a new project called `DoNotTrack`
+2. Create `.gitignore` file at root directory
+3. Create a java class under `src` called `WoodenSpoon`
+4. Enable version control to create new repo
+5. Make 4 commits
+6. Oops did not actually want to track this project
+7. All you have to do is
+   1. remove `.git` folder to remove the local repo
+   2. let intelliJ know this is not a git repo anymore
+ */
